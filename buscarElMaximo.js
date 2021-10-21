85e81706ba39393d084a511228ffb0d6018fe4be
Vittorio Adesso <adessovittorio8@gmail.com>
2021-10-21 13:02:53 +0200

    Iteración #1

---
+++ buscarElMaximo.js
@@ -0,0 +1,6 @@
+/* Completa la función que tomando dos números como argumento devuelva el más alto. */
+function sum(numberOne, numberTwo) {
+    // insert code
+    return Math.max(numberTwo, numberOne);
+}
+console.log(sum(50, 49));
